0d7fbbbf3ba2da3e7dbca8a7d74ee50e034c096d
Christopher W. Topel <ctopel@protonmail.ch>
2014-11-06 05:00:30 -0600

    Just the right number.

---
+++ forkids/justtherightnumber.py
@@ -0,0 +1,24 @@
+money = 420
+
+if money >= 100 and money <= 500 or money >= 1000 and money <= 5000:
+    print("We\'re within the limit.")
+
+if money >= 100 and money <= 500:
+    print("You\'re between 100 and 500. %s to be exact." % money)
+
+if money == 420:
+    print()
+    print('''      (               )       (            
+   (  )\ )   (     ( /(       )\ )  *   )  
+ ( )\(()/(   )\    )\())(    (()/(` )  /(  
+ )((_)/(_)|(((_)( ((_)\ )\    /(_))( )(_)) 
+((_)_(_))  )\ _ )\ _((_|(_)  (_)) (_(_())  
+ | _ ) |   (_)_\(_)_  /| __| |_ _||_   _|  
+ | _ \ |__  / _ \  / / | _|   | |   | |    
+ |___/____|/_/ \_\/___||___| |___|  |_|    
+                                           
+
+''')
+
+if money >= 1000 and money <= 5000:
+    print("You\'re between 1000 and 5000. %s to be exact." % money)
