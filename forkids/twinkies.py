1f67456a5acd2565881c17240d8b92515726414a
Christopher W. Topel <ctopel@protonmail.ch>
2014-11-06 05:00:23 -0600

    Twinkies

---
+++ forkids/twinkies.py
@@ -0,0 +1,13 @@
+twinkies = 421
+
+if twinkies < 100:
+    print("YOU NEED MORE TWINKIES. %s isn't enough!" % twinkies)
+
+elif twinkies > 500:
+    print("You have too many. Time to sell a few to fat people.")
+
+elif twinkies == 420:
+    print("BLAZE IT")
+
+else:
+    print("You've got plenty.")
