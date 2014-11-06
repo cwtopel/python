c1f03eb93942d1b1f91531f3790b0a1fafdf4d6a
Christopher W. Topel <ctopel@protonmail.ch>
2014-11-06 05:00:36 -0600

    If/Age

---
+++ forkids/ifage.py
@@ -0,0 +1,26 @@
+age = 0
+chris = 26
+
+if age == chris:
+    print()
+    print('You are me.')
+    
+elif age > 22:
+    print('You are old!')
+    print('You\'re only getting older.')
+    print('You\'ll probably die alone.')
+    print()
+    print('Just like me.')
+
+elif age < 26 and age > 0:
+    print()
+    print('You\'re still young.')
+    print('Too young to understand.')
+    print()
+    print('Don\'t envy me.')
+
+else:
+    print("Huh?")
+
+    
+
