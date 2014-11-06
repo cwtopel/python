ecffeff70f2fb6e137dfab7ffc14e26e17f44cb9
Christopher W. Topel <ctopel@protonmail.ch>
2014-11-06 05:19:54 -0600

    Can I fight those ninjas?

---
+++ forkids/fightninjas.py
@@ -0,0 +1,10 @@
+ninjas = 51
+
+if ninjas >= 50:
+    print("That's too many. I can't fight all those ninjas.")
+
+elif ninjas < 50 and ninjas >= 30:
+    print("It'll be a struggle, but I can take 'em.")
+
+elif ninjas < 30 and ninjas > 0:
+    print("I can fight those ninjas!")
