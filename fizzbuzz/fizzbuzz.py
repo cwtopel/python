d4b5ed1afea711e5e3defb30d3cbe5a2ccba7633
Christopher W. Topel <cwtopel@users.noreply.github.com>
2014-11-06 06:26:29 -0600

    Update fizzbuzz.py

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,8 +1,9 @@
 for i in range(1, 101):
-    if i%3==0:
-	    print((i),'Fizz')
-    if i%5==0:
-	    print((i),'Buzz')
     if (i%5==0 and i%3==0):
-	    print((i), "FizzBuzz")
-    print
+        print((i), "FizzBuzz")
+    elif i%3==0:
+        print((i),'Fizz')
+    elif i%5==0:
+        print((i),'Buzz')
+    else:
+        print(i)
