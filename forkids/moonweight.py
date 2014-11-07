4e5369a813cb6c8f3ee6c280b33879e8a484d937
Christopher W. Topel <ctopel@protonmail.ch>
2014-11-07 08:10:39 -0600

    Moon Weight
    Updated to involve user input and functions.

--- forkids/moonweight.py
+++ forkids/moonweight.py
@@ -1,5 +1,9 @@
-weight = 200
-
-for x in range(1, 16):
-    print('Year %s you would weigh %slbs.' % (x, (weight * 0.165)))
-    weight = weight + 2.5
+def moonweight():
+    print('How much do you weigh?')
+    weight = int(sys.stdin.readline())
+    for x in range(1, 16):
+        print('Year %s you would weigh %slbs.' % (x, (weight * 0.25)))
+        weight = weight + 2.5
+        
+moonweight()
+## print(moonweight()) returns None -- just call the function directly here
