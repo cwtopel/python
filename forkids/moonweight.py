e3c6a2bfb63df883f4cabf68e68344d7507c1fc4
Christopher W. Topel <ctopel@protonmail.ch>
2014-11-07 11:39:58 -0600

    Moon Weight
    Updated with new functions.

--- forkids/moonweight.py
+++ forkids/moonweight.py
@@ -1,9 +1,19 @@
+import sys
+
 def moonweight():
     print('How much do you weigh?')
     weight = int(sys.stdin.readline())
-    for x in range(1, 16):
+    
+    print('How long are you staying?')
+    years = int(sys.stdin.readline())
+    
+    print('How much do you plan to gain per year?')
+    addlbs = int(float(sys.stdin.readline()))
+    
+    for x in range(1, years):
         print('Year %s you would weigh %slbs.' % (x, (weight * 0.25)))
-        weight = weight + 2.5
+        weight = weight + addlbs
         
 moonweight()
 ## print(moonweight()) returns None -- just call the function directly here
+
